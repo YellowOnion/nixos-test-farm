f512093cf868f759ff555b13d3a3caa1d3002373
Daniel Hill <daniel@gluo.nz>
2022-10-31 19:36:14 +1300

    fix setup.sh

--- setup.sh
+++ setup.sh
@@ -6,10 +6,15 @@ if [[ ! -v 1 ]]; then
     exit 1
 fi
 nixos-generate-config --root /mnt --dir ./tmp-config
+
+# Replace host name.
 sed -i 's/# networking.hostName = "nixos";/ networking.hostName = "'$1'";/' ./tmp-config/configuration.nix
 
-mv ./tmp-config/configuration.nix ./$1.nix
-mv ./tmp-config/hardware-configuration.nix ./$1-hw.nix
+# import common.nix
+sed -i '/^      ./hardware-configuration.nix.*/a \        ./common.nix' ./tmp-config/configuration.nix
 
-ln -s $1.nix configuration.nix
-ln -s $1-hw.nix hardware-configuration.nix
+mv ./tmp-config/configuration.nix "./$1.nix"
+mv ./tmp-config/hardware-configuration.nix "./$1-hw.nix"
+
+ln -s "$1.nix" configuration.nix
+ln -s "$1-hw.nix" hardware-configuration.nix
